c3252d012abe2764ed8c0c014ff03cbe6d7cf6a6
ritchaves <rita.c.ferreira@live.com>
2017-04-26 21:36:18 +0100

    lab de cripto semi-done (falta testes)

--- security/src/main/java/org/komparator/security/CryptoUtil.java
+++ security/src/main/java/org/komparator/security/CryptoUtil.java
@@ -7,6 +7,46 @@ import java.util.*;
 
 public class CryptoUtil {
 
-    // TODO add security helper methods
+	
+	/*
+	 *  asymCipher() que recebe dados (byte[]) e uma chave e devolve esses dados cifrados. 
+	*/
+	public static byte[] asymCipher(byte[] plainBytes, PublicKey publicKey) throws NoSuchAlgorithmException, NoSuchPaddingException, 
+		InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+			byte[] cipherBytes = cipher.doFinal(plainBytes);
+			return cipherBytes;
+	}
+	
+	/*
+	 * asymDecipher() na classe que recebe os dados cifrados e uma chave e devolve os dados decifrados. 
+	 */
+	public static byte[] asymDecipher(byte[] bytes, PrivateKey privateKey) throws NoSuchAlgorithmException, NoSuchPaddingException, 
+	InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+		byte[] decipherBytes = cipher.doFinal(bytes); 
+		return decipherBytes;
+	}
 
+	// digital signature --------
+	
+	public static byte[] makeDigitalSignature(final PrivateKey privateKey, final byte[] bytesToSign) throws NoSuchAlgorithmException, 
+		InvalidKeyException, SignatureException {
+		Signature signature = Signature.getInstance("SHA256withRSA");
+		signature.initSign(privateKey);
+		signature.update(bytesToSign);
+		byte[] signatureResult = signature.sign();
+		return signatureResult;
+	}
+	
+	public static boolean verifyDigitalSignature(PublicKey publicKey, byte[] bytesToVerify, byte[] signature) throws NoSuchAlgorithmException,
+		InvalidKeyException, SignatureException {
+		Signature sig = Signature.getInstance("SHA256withRSA");
+		sig.initVerify(publicKey);
+		sig.update(bytesToVerify);
+		return sig.verify(signature);
+	}	
+	
 }
